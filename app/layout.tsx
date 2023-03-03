4b62463344a91bb5b7c75db06b1b24217983cf74
Cristian Orozco <cristian.orozco@globant.com>
2023-03-03 08:47:02 -0500

    Add pizza list

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import Featured from '@/components/Featured'
 import Footer from '@/components/Footer'
 import Navbar from '@/components/Navbar'
+import PizzaList from '@/components/PizzaList'
 import '../styles/globals.css'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
@@ -12,6 +13,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body>
         <Navbar />
         <Featured />
+        <PizzaList />
         {children}
         <Footer />
       </body>
